4c4a2568ac4ce75162ea0ff0ef307b6f8c622cca
gaveranubhav-cpu <gaveranubhav@gmail.com>
2026-01-16 16:14:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/AllStoriesviparmy') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/infinityaudiosearchinggroup') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hdfilm0900_bot') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/storyupdatechannel') # Update channel link for the bot
 
